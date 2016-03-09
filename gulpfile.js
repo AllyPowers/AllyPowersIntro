be3862f81a46545e8013ae0c0fbb1cc00e4b5db4
lancelou <lc1456666@outlook.com>
2016-03-09 21:59:01 +0800

    all of files about index.html initiate

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var less = require('gulp-less');
 var connect = require('gulp-connect');
 var LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var db = levelup('./.cache');
+var sass = require('gulp-sass');
 
 var srcRoot = 'src';
 
@@ -19,6 +20,13 @@ gulp.task('connect', function () {
         livereload: true,
         fallback: 'src/index.html'
     });
+});
+
+// compile Sass
+gulp.task('sass', function() {
+    gulp.src(srcRoot + '/css/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(srcRoot + '/css'));
 });
 
 gulp.task('watch-html', function () {
@@ -35,5 +43,17 @@ gulp.task('watch-js', function () {
     });
 });
 
-gulp.task('watch', ['watch-html','watch-js'])
+gulp.task('watch-css', function(){
+    // 监听文件变化
+    gulp.watch(srcRoot + '/css/sass/*.scss', function(){
+        gulp.run('sass');
+        return gulp.src(srcRoot + '/css/sass/*.scss')
+            .pipe(connect.reload());
+    });
+});
+
+gulp.task('watch', ['watch-html','watch-js','watch-css'])
 gulp.task('default', ['connect', 'watch']);
+
+
+
