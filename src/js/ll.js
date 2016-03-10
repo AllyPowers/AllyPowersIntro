3bff77c929a463b7413fc07181cdeaf31a19115b
qtpalmtop <1762696383@qq.com>
2016-03-10 18:48:52 +0800

    李林页面完成。

--- src/js/ll.js
+++ src/js/ll.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-	
+	$(document).ready(function(){
+		
+		$("#menu").click(function(){
+			$("#menu").hide(500);
+			$("#menu-section").css({"display":"block","transform":"translate(0px,0px)"});
+/* 			var h=$(".title-nav").css("top");
+			alert(h); */
+		});
+		$(".close").click(function(){
+			$("#menu-section").css({"display":"block","transform":"translate(240px,0px)"});
+			$("#menu").show(500);
+		});
+	});
 }
