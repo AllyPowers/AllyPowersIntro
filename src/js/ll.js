10e1071b81d138a51a8c05cd2c5b7f246288c73e
qtpalmtop <1762696383@qq.com>
2016-03-14 13:52:48 +0800

    增加背景音乐2

--- src/js/ll.js
+++ src/js/ll.js
@@ -3,12 +3,12 @@ window.onload = function(){
 		
 		$("#menu").click(function(){
 			$("#menu").hide(500);
-			$("#menu-section").css({"display":"block","transform":"translate(0px,0px)"});
+			$("#menu-section").css({"display":"block","transform":"translate(0px,0px)","opacity":"1"});
 /* 			var h=$(".title-nav").css("top");
 			alert(h); */
 		});
 		$(".close").click(function(){
-			$("#menu-section").css({"display":"block","transform":"translate(240px,0px)"});
+			$("#menu-section").css({"display":"block","transform":"translate(240px,0px)","opacity":"0"});
 			$("#menu").show(500);
 		});
 	});
